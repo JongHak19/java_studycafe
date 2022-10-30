e67278fde06d75c7d9603aa05ed8e05dec359342
JongHak19 <105853600+JongHak19@users.noreply.github.com>
2022-10-30 14:29:39 +0900

    upload MainFrame.java

---
+++ GUI/관리자/MainFrame.java
@@ -0,0 +1,61 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainFrame extends JFrame{
+	Color back = new Color(0x030066);
+	Color but = new Color(0xC9C6FF);
+	Font font = new Font("맑은 고딕", Font.BOLD, 20);
+	
+	public MainFrame(){
+		setTitle("스터디 카페");
+		setSize(1300,800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		Dimension frameSize = getSize();
+		 
+        Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((windowSize.width - frameSize.width) / 2, //화면 중앙에 메인 프레임 띄우기
+                (windowSize.height - frameSize.height) / 2);
+        
+		c.setBackground(back);		
+		c.setLayout(null);	
+    
+    
+    
+		//관리자 gui 
+		ImageIcon img = new ImageIcon("./image/admin.png");
+		JButton adminb = new JButton(img);
+		adminb.setBounds(1200, 600, 30, 30);
+		c.add(adminb);
+		adminb.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {	
+				AdminPanel adminp = new AdminPanel();
+				c.add(adminp);
+				adminb.setVisible(false);
+			}
+		});
+		//여기까지 다른 패널이 없어서 임의로 추가한 부분
+		setVisible(true);
+
+	}
+	
+	class AdminPanel extends JPanel{
+		JButton SeatStatus = new JButton("좌석 현황");
+		JButton member = new JButton("회원 관리");
+		JButton sales = new JButton("매출");
+		public AdminPanel() {
+			setSize(1100,700);
+			setBackground(but);
+			setVisible(true);
+			this.add(SeatStatus);
+			this.setLocation(90, 30);	
+			setVisible(true);
+		}
+		
+	}
+	public static void main(String [] args) {
+		new MainFrame();
+	}
+}
